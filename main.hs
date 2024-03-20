13ac7eb2b450b81a22479d76503006547bb90201
Gonzalo Lafuente <glafuente@dc.uba.ar>
2024-03-20 16:17:55 -0300

    tp-0 por la mitad

---
+++ main.hs
@@ -0,0 +1,45 @@
+import Data.Maybe
+import Data.Either
+import Data.String
+
+{-
+  Punto 2 (falta el d)  
+-}
+
+valorAbsoluto :: Float -> Float
+valorAbsoluto x | x<0 = -(x)
+                |otherwise = x
+
+
+esBisiesto :: Integer -> Bool
+esBisiesto x | x `mod` 400 == 0 = True
+             | x `mod` 100 /= 0 && x `mod` 4 == 0 = True
+             | otherwise = False
+
+factorial :: Integer -> Integer
+factorial 0 = 1
+factorial x = x * factorial(x-1)
+
+{-
+  Punto 3 (problema para probar el b)  
+-}
+
+inverso :: Float -> Maybe Float
+inverso x | (1/x) * x == 1 = Just (1/x)
+          | otherwise = Nothing  
+
+aEntero :: Either Integer Bool -> Integer
+aEntero (Right x) | x = 1
+                  |otherwise = 0
+aEntero (Left y) = y
+
+{-
+    Punto 4
+-}
+
+esta :: String -> String -> Bool
+
+
+limpiar :: String -> String -> String 
+limpiar x y = filter esta y x
+    
