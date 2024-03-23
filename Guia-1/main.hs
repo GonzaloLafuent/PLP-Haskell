313cc74dfe0eb7ad62e416431639016bbe29ff2a
Gonzalo Lafuente <lafuentegonzalo1@gmail.com>
2024-03-23 18:38:33 -0300

    punto 3 terminado

--- Guia-1/main.hs
+++ Guia-1/main.hs
@@ -26,14 +26,42 @@ concat_2 :: [a] -> [a] -> [a]
 concat_2 a b = foldr (\x r-> x:r) b a
 
 filter_2 :: (a->Bool) -> [a] -> [a]
-filter_2 p l = foldr( 
-                    \x r -> if p x then x:r
-                            else r      
-                ) [] l
+filter_2 p l = foldr( \x r -> if p x then x:r else r) [] l 
 
 map_2 :: (a -> b) -> [a] -> [b]
-map_2 f l = foldr(\x r -> (f x):r) [] l
+map_2 f = foldr(\x r -> (f x):r) [] 
 
-{-a-}
+{-b-}
 mejor_segun :: (a -> a -> Bool) -> [a] -> a
-mejor_segun p l = foldr1 (\x r->) Nil l
+mejor_segun p l = foldr1 (\x r-> if p x r then x else r ) l
+
+{-c-}
+sumas :: Num a => [a] -> [a]
+sumas [] = []
+sumas (x:xs) = [x + (sum xs)] ++ sumas xs
+
+sumasParciales :: Num a => [a] -> [a]
+sumasParciales l = reverse( sumas (reverse l))
+
+{-d-}
+sumaAlt :: Num a => [a] -> a
+sumaAlt l = foldr (-) 0 l
+
+{-
+        foldr (-) 0 [1,2,3,4] = (-) 1 foldr (-) 0 [2,3,4]
+                              = (-) 1 ((-) 2 foldr (-) 0 [3,4])
+                              = (-) 1 ((-) 2 ((-) 3 foldr (-) 0 [4]))
+                              = (-) 1 ((-) 2 ((-) 3 ((-) 4 foldr (-) 0 [])))
+                              = (-) 1 ((-) 2 ((-) 3 ((-) 4 0)))
+                              = (-) 1 ((-) 2 ((-) 3 (4-0)))
+                              =  1 - ( 2 -( 3 - (4-0)))
+                              =  1 - ( 2 - 3 + 4)
+                              =  1 - 2 + 3  - 4 = -2  
+        (Diapo 20 de la teorica 1 ejemplifica bien de acuerdo a como se asocian las operaciones)                      
+-}
+{-e-}
+sumaAltInverse :: Num a => [a] -> a
+sumaAltInverse l = foldr (-) 0 (reverse l)  
+
+{-4-}
+{-a-}
